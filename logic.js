ba744c031e220e1a89b7f0c7cd20163f04821568
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-26 17:44:43 +0530

    FIX:0,3,6

--- logic.js
+++ logic.js
@@ -37,7 +37,7 @@ let winnerIndicator=getComputedStyle(document.body).getPropertyValue('--winning-
         currentplayer=(currentplayer == x_text ? o_text:x_text);
     }
  }
- const prob=[[0,1,2],[0,3,4],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]]
+ const prob=[[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]]
  function playerHasWon(){
     for(const condition of prob){
         let [a,b,c]=condition
